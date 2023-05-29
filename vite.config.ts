6654a573e53383d54a797faecd738b634b3a8aab
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2023-05-29 07:42:35 +0200

    Properly add news images where they should be

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import type { UserConfig } from "vite";
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
-	server: {
-		fs: {
-			// Allow serving files outside of the project root
-			allow: ["./images/news"],
-		},
-	},
 };
 
 export default config;
