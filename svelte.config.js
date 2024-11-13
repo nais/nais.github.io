4e8277072366be1250c0efaca325a938cc4d0655
frodesundby <frode.sundby@nav.no>
2024-11-13 13:45:07 +0100

    prerender everything
    Co-authored-by: andreas Nordahl <andreas.nordahl@nav.no>

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,37 @@
-import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
+import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
+	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: preprocess({ postcss: true }),
 
 	kit: {
 		adapter: adapter(),
-		outDir: "build",
+
+		alias: {
+			$lib: path.resolve(".", "src", "lib"),
+		},
 	},
 };
 
 export default config;
+
+// import adapter from "@sveltejs/adapter-auto";
+// import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://svelte.dev/docs/kit/integrations
+// 	// for more information about preprocessors
+// 	preprocess: vitePreprocess(),
+
+// 	kit: {
+// 		adapter: adapter(),
+// 		outDir: "build",
+// 	},
+// };
+
+// export default config;
