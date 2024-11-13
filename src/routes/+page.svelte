673e81a68daf6815c3eee75ac2b5e496a601c637
frodesundby <frode.sundby@nav.no>
2024-11-13 14:36:33 +0100

    swap glances and users

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script>
-	import Brukere from '$lib/Brukere.svelte';
-	import Hero from '$lib/Hero.svelte';
-	import Glances from '$lib/Glances.svelte';
+	import Brukere from "$lib/Brukere.svelte";
+	import Glances from "$lib/Glances.svelte";
+	import Hero from "$lib/Hero.svelte";
 </script>
 
 <Hero />
-
-<Brukere />
-
 <Glances />
+<Brukere />
