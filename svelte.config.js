87a90bf84b4383a7c073f0f5c030ca6c13523324
frodesundby <frode.sundby@nav.no>
2024-11-14 10:02:11 +0100

    remove postcss preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ import path from "path";
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({ postcss: true }),
+	preprocess: preprocess(),
 
 	kit: {
 		adapter: adapter(),
