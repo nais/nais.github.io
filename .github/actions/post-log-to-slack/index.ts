0478a6040c84f269ee1f5ec129de2f02ba07516f
Andreas Nordahl <andnorda@gmail.com>
2024-11-22 14:01:37 +0100

    reference payload as env

--- .github/actions/post-log-to-slack/index.ts
+++ .github/actions/post-log-to-slack/index.ts
@@ -4,7 +4,11 @@ const token = process.env.SLACK_TOKEN;
 
 const web = new WebClient(token);
 
+const text = `Hello from github action! ${process.env.GITHUB_CONTEXT}, ${process.env.VARS_CONTEXT}`;
+
+console.log(text);
+
 web.chat.postMessage({
 	channel: "nais-test-announcements",
-	text: "Hello from github action!",
+	text,
 });
