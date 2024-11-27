8f188de3531f64cf80976c9a96199bfc3a576cc5
frodesundby <frode.sundby@nav.no>
2024-11-27 11:13:40 +0100

    fixed path to 404 logo

--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -1,11 +1,12 @@
-<img class="centered" src="logo404.png" alt=""/>
+<img class="centered" src="/logo404.png" alt="nais logo with 404" />
+
 <style>
-    .centered {
-    text-align: center;
-    position: absolute;
-    top: 20%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 300px;
-}
+	.centered {
+		text-align: center;
+		position: absolute;
+		top: 20%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		width: 300px;
+	}
 </style>
