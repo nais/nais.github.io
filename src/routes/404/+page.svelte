9d21eff26df9d26c931f2ba813ebc2eb77fb9b5d
frodesundby <frode.sundby@nav.no>
2024-11-27 11:07:51 +0100

    add custom 404

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,11 @@
+<img class="centered" src="logo404.png" alt=""/>
+<style>
+    .centered {
+    text-align: center;
+    position: absolute;
+    top: 20%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 300px;
+}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -39,6 +39,7 @@ const config = {
 		adapter: adapter({
 			pages: "build",
 			assets: "build",
+			fallback: "404.html",
 		}),
 
 		alias: {
