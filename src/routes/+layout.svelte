e77a1568fb6f481dcc1780fdb4d164efeff8f106
Kyrre Havik <kyrre.havik@nav.no>
2025-08-12 09:38:34 +0200

    feat: Amplitude er på vei ut i Nav
    Vi har mulighet for å sette opp Umami, men da må vi få lagd en cookie-banner.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,7 @@
 <script lang="ts">
 	import { browser } from "$app/environment";
 	import Header from "$lib/Header.svelte";
-	import * as amplitude from "@amplitude/analytics-browser";
 	import "../styles/app.css";
-
-	if (browser) {
-		const getApiKey = () => {
-			if (window.location.href.includes("nais.io")) {
-				return "16d1ee2fd894ca2562eeebb5095dbcf0"; // prod
-			}
-			return "04203d48401492bda4620a74acf85a5b"; // dev
-		};
-
-		amplitude.init(getApiKey(), {
-			serverUrl: "https://amplitude.nav.no/collect",
-			serverZone: "EU",
-			defaultTracking: true,
-			identityStorage: "localStorage",
-		});
-	}
 </script>
 
 <Header />
